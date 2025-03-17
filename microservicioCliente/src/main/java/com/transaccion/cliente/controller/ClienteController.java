31eb015e55e956b9ba382ea093fb4c1fe2c04dd6
BryanArmando <bryanquisaguano@gmail.com>
2025-03-17 02:40:34 -0500

    Documentación de endpoints con swagger

--- microservicioCliente/src/main/java/com/transaccion/cliente/controller/ClienteController.java
+++ microservicioCliente/src/main/java/com/transaccion/cliente/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.transaccion.cliente.controller;
 import com.transaccion.cliente.dto.ClienteRequestDto;
 import com.transaccion.cliente.dto.ClienteResponseDto;
 import com.transaccion.cliente.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,26 +21,63 @@ import java.util.List;
 @RestController
 @RequestMapping("api/cliente")
 @CrossOrigin(value = "*")
+@Tag(name = "Cliente", description = "Cliente API")
 public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
 
+    @Operation(summary = "Obtiene una lista de clientes activos",
+            description = "Servicio que obtiene una listado de clientes activos"
+    )
     @GetMapping
     public ResponseEntity<List<ClienteResponseDto>> buscarClientesActivos(){
         return ResponseEntity.ok(clienteService.clientes());
     }
 
+    @Operation(summary = "Realiza el guardado de clientes",
+            description = "Servicio que guarda un cliente y retorna los datos del cliente guardado",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Objeto Dto de tipo `Cliente`, "
+                    + "con datos de cliente a guardar.<br>",
+                    required = true
+            )
+    )
     @PostMapping("/save")
     public ResponseEntity<ClienteResponseDto> crearCliente(@RequestBody @Valid ClienteRequestDto clienteRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.guardarCliente(clienteRequestDto));
     }
 
+    @Operation(summary = "Realiza una edicion de un cliente activo",
+            description = "Servicio que realiza la edición de un cliente mediante un id y sus respectivos datos a editar.",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "Código único de cliente a editar.",
+                            required = true,
+                            example = "1"
+                    )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Objeto Dto de tipo `Cliente`, "
+                    + "con los datos a editar.<br>",
+                    required = true
+            )
+    )
     @PostMapping("/{id}")
     public ResponseEntity<ClienteResponseDto> editarCliente(@PathVariable("id") Integer id, @RequestBody @Valid ClienteRequestDto clienteRequestDto){
         return ResponseEntity.ok(clienteService.editarDatosCliente(id, clienteRequestDto));
     }
 
+    @Operation(summary = "Realiza una eliminación lógica de un cliente activo",
+            description = "Servicio que realiza una eliminación lógica del cliente cambiando su estado activo (1) a inactivo (0).",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "Código único de cliente a eliminar.",
+                            required = true,
+                            example = "1"
+                    )
+            }
+    )
     @PostMapping("/eliminar/{id}")
     public ResponseEntity<String> inactivarCliente(@PathVariable("id") Integer id){
         clienteService.inactivarCliente(id);
--- microservicioCuenta/src/main/java/com/transaccion/cuenta/controller/MovimientoController.java
+++ microservicioCuenta/src/main/java/com/transaccion/cuenta/controller/MovimientoController.java
@@ -4,6 +4,9 @@ import com.transaccion.cuenta.dto.*;
 import com.transaccion.cuenta.dto.reportes.EstadoCuentaCabeceraReporteDto;
 import com.transaccion.cuenta.service.MovimientoService;
 import com.transaccion.cuenta.service.reportes.EstadoCuentaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("api/movimientos")
+@Tag(name = "Movimientos", description = "Movimientos API")
 public class MovimientoController {
     @Autowired
     private MovimientoService movimientoService;
@@ -26,11 +30,30 @@ public class MovimientoController {
     @Autowired
     private EstadoCuentaService estadoCuentaService;
 
+    @Operation(summary = "Realiza el guardado de movimientos",
+            description = "Servicio que realiza el registro de un movimiento relacionado a una cuenta.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Objeto Dto de tipo `Movimiento`, "
+                    + "con datos del movimiento(`idCuenta`, `valor`) a guardar.<br>"
+                    + "El campo `valor` al ser negativo se considera un RETIRO y al ser positivo es un DEPOSITO ",
+                    required = true
+            )
+    )
     @PostMapping("/save")
     public ResponseEntity<MovimientoResponseDto> registrarMovimiento(@RequestBody @Valid MovimientoRequestDto movimientoRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(movimientoService.registrarMovimiento(movimientoRequestDto));
     }
 
+    @Operation(summary = "Realiza búsqueda de movimientos",
+            description = "Servicio que realiza la búsqueda de los movimientos realizados en la cuenta.",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "Código único de cuenta.",
+                            required = true,
+                            example = "1"
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<List<MovimientoResponseDto>> obtenerMovimientosCuenta(@PathVariable("id") Integer id){
         return ResponseEntity.ok(movimientoService.listadoMovimiento(id));
@@ -43,6 +66,29 @@ public class MovimientoController {
      * @param fechaFin
      * @return
      */
+    @Operation(summary = "Realiza la generación de reportes para estado de cuenta",
+            description = "Servicio que realiza la generacion de un estado de cuenta de un cliente en un rango de fechas.",
+            parameters = {
+                    @Parameter(
+                            name = "clienteId",
+                            description = "Código único de cliente.",
+                            required = true,
+                            example = "1"
+                    ),
+                    @Parameter(
+                            name = "fechaInicio",
+                            description = "Fecha inicial de filtro.",
+                            required = true,
+                            example = "2025-03-01"
+                    ),
+                    @Parameter(
+                            name = "fechaFin",
+                            description = "Fecha final de filtro.",
+                            required = true,
+                            example = "2025-04-01"
+                    )
+            }
+    )
     @GetMapping("/reportes")
     public ResponseEntity<EstadoCuentaCabeceraReporteDto> obtenerEstadoDeCuenta(
             @RequestParam("clienteId") Integer clienteId,
